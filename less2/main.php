15b7d4a6e4fed13d38087427d6f138db55dc2376
dimich47 <dima_gatch@mail.ru>
2019-12-19 10:58:42 +0300

    ver2

--- less2/main.php
+++ less2/main.php
@@ -43,6 +43,18 @@ echo 'Результат деления = ', $num, ' на ', $i, '-й итера
 
 echo ('<br>');
 echo '<br>', 'Задача 4', '<br>';
+
+//      4.1
+$title = 'C++';
+$pageCount = '136';
+$book = [
+    'title' => 'PHP 7',
+    'pageCount' => 342
+];
+extract($book, EXTR_OVERWRITE);
+echo $title, ' -', $pageCount, '<br>' ;
+
+//      4.2
 $arr1 = [1,2,3];
 $arr2 = [
             'id' =>1,
@@ -50,20 +62,74 @@ $arr2 = [
 ];
 echo 'В массиве arr1 - ', count($arr1), ' элемента(тов)', '<br>';
 echo 'В массиве arr2 - ', count($arr2), ' элемента(тов)', '<br>';
+echo ('<br>');
 
-echo 'В массиве arr1 значение 7 находится в ячейке ', array_search(7, $arr1),  '<br>';
+//      4.3
+if(in_array("7", $arr1))
+echo 'В массиве arr1 значение 7 есть', '<br>';
+else
+echo 'В массиве arr1 значение 7 отсуствует', '<br>';
 echo 'В массиве arr2 значение user находится в ячейке ', array_search(user, $arr2),  '<br>';
-echo ('Замена содержимого массива1 содержимым массива 3');
-$arr3 = [4,5,6];
-$arr1 = array_replace($arr1, $arr3);
-var_dump( $arr1);
+echo ('<br>');
 
+//      4.4
+echo ('Замена содержимого массива1 содержимым массива 3'),'<br>';
+$arr3 = [4,5,6];
+echo 'Был массив: ';
+var_dump( $arr1);
+$arr1 = array_replace($arr1, $arr3);
+echo  '<br>', 'Стал массив: ';
+var_dump( $arr1);
+echo ('<br>');
+echo ('<br>');
+//      4.5
+echo 'Cоздание массива, содержащего название переменнных и их значение';
 $id = '123';
 $login = 'Ivan';
 echo ('<br>');
 $arr4 = compact('id',"login");
-echo 'Новый массив ';
+echo 'Новый массив: ';
 var_dump( $arr4);
-
 echo ('<br>');
-echo '<br>', 'Задача 5', '<br>';
+
+//      5
+echo '<br>', 'Задача 5', '<br>', 'Сортировка массива $myarr по  \'price\'','<br>';
+$myArr = [
+    '01'=> [
+        'price' => 10,
+        'count' => 2
+    ],
+    '02'=> [
+        'price' => 5,
+        'count' => 5
+    ],
+    '03'=> [
+        'price' => 8,
+        'count' => 15
+    ],
+    '04'=> [
+        'price' => 12,
+        'count' => 4
+    ],
+    '05'=> [
+        'price' => 8,
+        'count' => 44
+    ],
+];
+
+// сортировка по 'price'
+// создаем массив 'newarr', в который переместим из 'myarr' все значения 'price'
+// затем через функцию 'array_multisort' отсортируем 'myarr' по  'newarr' (вначале сортируется по возрастанию массив 'newarr')
+$newArr = [];
+foreach ($myArr as $key=> $data)
+{
+    $newArr[$key] = $data['price'];
+}
+array_multisort($newArr, $myArr);
+//var_dump($myArr);
+foreach ($myArr as  $data)
+{
+    echo 'price= ', $data['price'], '  count= ';
+    echo $data['count'], '<br>';
+}
+
